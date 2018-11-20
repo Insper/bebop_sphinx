416d602fc0f08811f0d5e0859c5c0792497aae4c
Fabio de Miranda <fmiranda@gmail.com>
2018-11-20 16:59:33 -0200

    bebop firmware

--- fim_bebop_firmwared_down.sh
+++ fim_bebop_firmwared_down.sh
@@ -2,7 +2,8 @@ fdc drop_all instances
 fdc drop_all firmwares
 rm /dev/shm/shd_*
 sudo systemctl stop firmwared.service
-sudo rm /usr/share/firmwared/firmwares/*
+# uncomment line below only in case of buggy firmwares
+#sudo rm /usr/share/firmwared/firmwares/*
 
 
 
