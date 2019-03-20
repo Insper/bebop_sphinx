d0c5dd22601c751d3a734b47b09d6a8ac9c12a6a
mirwox <fmiranda@gmail.com>
2019-03-20 15:26:05 -0300

    scripts

---
+++ mavros/roda_px4.sh
@@ -0,0 +1,5 @@
+cd ~/src/Firmware
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
+roslaunch -v mavros px4.launch fcu_url:="udp://:14540@127.0.0.1:14557"
